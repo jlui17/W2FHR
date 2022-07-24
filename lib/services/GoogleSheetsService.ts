2ce3d89b0102c5b5cded9c518f8631377a5052b1
jlui17 <justinlui17@gmail.com>
2022-07-23 23:05:05 -0700

    Add secret value retrival permissions to test hanlder in google sheets service

--- lib/services/GoogleSheetsService.ts
+++ lib/services/GoogleSheetsService.ts
@@ -1,5 +1,6 @@
 import { GoFunction } from "@aws-cdk/aws-lambda-go-alpha";
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
 export class GoogleSheetsService extends Stack {
@@ -13,5 +14,12 @@ export class GoogleSheetsService extends Stack {
       entry: `${FUNCTION_FOLDER}/packages`,
       moduleDir: `${FUNCTION_FOLDER}/go.mod`,
     });
+    this.testHandler.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["secretsmanager:GetSecretValue"],
+        effect: Effect.ALLOW,
+        resources: [this.testHandler.functionArn],
+      })
+    );
   }
 }
