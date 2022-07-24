cc532056b1d2bd21abf16305c8b063b1493adb62
jlui17 <justinlui17@gmail.com>
2022-07-23 23:39:05 -0700

    Pass in API key as env to test handler

--- src/GoogleSheets/go.mod
+++ src/GoogleSheets/go.mod
@@ -2,12 +2,4 @@ module GoogleSheets
 
 go 1.18
 
-require (
-	github.com/aws/aws-lambda-go v1.33.0
-	github.com/aws/aws-sdk-go v1.19.23
-)
-
-require (
-	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-)
+require github.com/aws/aws-lambda-go v1.33.0
--- src/GoogleSheets/packages/main.go
+++ src/GoogleSheets/packages/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-
-	"GoogleSheets/packages/common/secrets"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -18,16 +17,9 @@ func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) eve
 			Body:       "No name",
 		}
 	}
-	apiKey, err := secrets.GetGoogleSheetsApiKey()
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       err.Error(),
-		}
-	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: 201,
-		Body:       fmt.Sprintf("Hello %s! %s", name, apiKey),
+		Body:       fmt.Sprintf("Hello %s! %s", name, os.Getenv("GOOGLE_API_KEY")),
 	}
 }
 
