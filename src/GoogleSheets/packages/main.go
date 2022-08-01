329a4989e30b06cae2bf2b03ffd72e9001047e05
jlui17 <justinlui17@gmail.com>
2022-08-01 03:37:02 -0700

    Setup client and read simple cell value

--- src/GoogleSheets/packages/main.go
+++ src/GoogleSheets/packages/main.go
@@ -3,28 +3,39 @@ package main
 import (
 	googleClient "GoogleSheets/packages/common/GoogleClient"
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
 func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	client, err := googleClient.GetClient()
+	client := googleClient.GetReadOnlyClient()
+
+	sheetsService, err := sheets.NewService(context.Background(), option.WithHTTPClient(client))
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body:       err.Error(),
+			Body:       fmt.Sprintf("Error creating sheets service: %s", err.Error()),
 		}, nil
 	}
-	if client != nil {
+
+	sheetId := "1Q9Wt-dtjJN3c3lcJ1v8SHTTz4molRm09rAru3BT8AV8"
+	readRange := "Master Timesheet!A2"
+
+	response, err := sheetsService.Spreadsheets.Values.Get(sheetId, readRange).Do()
+	if err != nil {
 		return events.APIGatewayProxyResponse{
-			StatusCode: 201,
-			Body:       "Success",
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Error reading sheet: %s", err.Error()),
 		}, nil
 	}
+
 	return events.APIGatewayProxyResponse{
-		StatusCode: 400,
-		Body:       "Bad",
+		StatusCode: 201,
+		Body:       fmt.Sprintf("%s", response.Values[0][0]),
 	}, nil
 }
 
