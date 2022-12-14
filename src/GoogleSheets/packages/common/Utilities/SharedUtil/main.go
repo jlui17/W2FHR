7d0f5f68e81653a23be2a0b3aacb9ca14d617375
Justin Lui <justinlui17@gmail.com>
2022-12-13 16:51:04 -0800

    Move LETTER_TO_NUMBER_MAP to SharedUtil

--- src/GoogleSheets/packages/common/Utilities/SharedUtil/main.go
+++ src/GoogleSheets/packages/common/Utilities/SharedUtil/main.go
@@ -1,9 +1,36 @@
 package SharedUtil
 
-import (
-	"GoogleSheets/packages/common/Constants/SharedConstants"
+var (
+	const_LETTER_TO_NUMBER_MAP = map[string]int{
+		"A": 0,
+		"B": 1,
+		"C": 2,
+		"D": 3,
+		"E": 4,
+		"F": 5,
+		"G": 6,
+		"H": 7,
+		"I": 8,
+		"J": 9,
+		"K": 10,
+		"L": 11,
+		"M": 12,
+		"N": 13,
+		"O": 14,
+		"P": 15,
+		"Q": 16,
+		"R": 17,
+		"S": 18,
+		"T": 19,
+		"U": 20,
+		"V": 21,
+		"W": 22,
+		"X": 23,
+		"Y": 24,
+		"Z": 25,
+	}
 )
 
 func GetIndexOfColumn(columnLetter string) int {
-	return SharedConstants.LETTER_TO_NUMBER_MAP[columnLetter]
+	return const_LETTER_TO_NUMBER_MAP[columnLetter]
 }
