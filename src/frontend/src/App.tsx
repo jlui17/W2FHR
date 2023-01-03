f3e959a097473ed83cefa28b6a3e408b36ab753e
Justin Lui <justinlui17@gmail.com>
2023-01-02 23:30:05 -0800

    Add react router basic layout

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,19 +1,32 @@
 import { CssBaseline, StyledEngineProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthenticationForm from "./components/AuthenticationForm";
 import AvailabilityForm from "./components/AvailabilityForm";
 import Timesheet from "./components/Timesheet";
 import UpcomingShifts from "./components/UpcomingShifts";
 
+const Dashboard = () => {
+  return (
+    <div>
+      <AvailabilityForm />
+      <UpcomingShifts />
+      <Timesheet />
+    </div>
+  );
+};
+
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <CssBaseline />
       <div className="App">
-        <AuthenticationForm />
-        <AvailabilityForm />
-        <UpcomingShifts />
-        <Timesheet />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AuthenticationForm />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </StyledEngineProvider>
   );
