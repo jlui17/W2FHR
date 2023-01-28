1ff781bf0d76578e1b2eff82560482ad19cfc6eb
Justin Lui <justinlui17@gmail.com>
2023-01-28 17:19:28 +0000

    Dashboard redirect to login if not logged in

--- src/frontend/src/components/Dashboard/DashboardController.tsx
+++ src/frontend/src/components/Dashboard/DashboardController.tsx
@@ -1,14 +1,23 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthenticationContext } from "../AuthenticationContextProvider";
 import AvailabilityForm from "../AvailabilityForm";
+import { ROUTES } from "../common/constants";
 import Timesheet from "../Timesheet";
 import UpcomingShifts from "../UpcomingShifts";
 
 const DashboardController = () => {
-  return (
+  const { getAuthSession } = useContext(AuthenticationContext);
+  const isLoggedin = getAuthSession() !== null;
+
+  return isLoggedin ? (
     <div>
       <AvailabilityForm />
       <UpcomingShifts />
       <Timesheet />
     </div>
+  ) : (
+    <Navigate to={ROUTES.LOGIN} />
   );
 };
 
