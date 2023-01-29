eaa365b83ecf30303706eae4f0635c0c65974ffd
Justin Lui <justinlui17@gmail.com>
2023-01-29 15:04:25 +0000

    Header fix logout

--- src/frontend/src/components/Dashboard/components/Header/HeaderController.tsx
+++ src/frontend/src/components/Dashboard/components/Header/HeaderController.tsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthenticationContext } from "../../../AuthenticationContextProvider";
+import { ROUTES } from "../../../common/constants";
 import { HeaderWidget } from "./HeaderWidget";
 
 const HeaderController = () => {
   const { logout } = useContext(AuthenticationContext);
+  const navigate = useNavigate();
 
   const onLogout = () => {
     logout();
-    window.location.reload();
+    navigate(ROUTES.LOGIN);
   };
 
   return <HeaderWidget onLogout={onLogout} />;
