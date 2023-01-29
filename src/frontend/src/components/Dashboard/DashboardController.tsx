dab49c614b34c23e3894c559675e3e633c4c0719
Justin Lui <justinlui17@gmail.com>
2023-01-29 14:14:25 +0000

    Dashboard remove lazyloading (useless) lol

--- src/frontend/src/components/Dashboard/DashboardController.tsx
+++ src/frontend/src/components/Dashboard/DashboardController.tsx
@@ -1,24 +1,13 @@
-import { lazy, Suspense, useContext } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthenticationContext } from "../AuthenticationContextProvider";
 import { ROUTES } from "../common/constants";
-
-const DashboardWidget = lazy(() =>
-  import("./DashboardWidget").then((module) => ({
-    default: module.DashboardWidget,
-  }))
-);
+import { DashboardWidget } from "./DashboardWidget";
 
 const DashboardController = () => {
   const { isLoggedIn } = useContext(AuthenticationContext);
 
-  return isLoggedIn() ? (
-    <Suspense fallback={null}>
-      <DashboardWidget />
-    </Suspense>
-  ) : (
-    <Navigate to={ROUTES.LOGIN} />
-  );
+  return isLoggedIn() ? <DashboardWidget /> : <Navigate to={ROUTES.LOGIN} />;
 };
 
 export const Dashboard = () => {
