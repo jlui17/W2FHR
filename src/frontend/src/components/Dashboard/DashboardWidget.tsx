14671102519e3e9048be0de8710f01696668d63b
Justin Lui <justinlui17@gmail.com>
2023-02-01 14:34:03 +0000

    DashboardWidget darken background

--- src/frontend/src/components/Dashboard/DashboardWidget.tsx
+++ src/frontend/src/components/Dashboard/DashboardWidget.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 
 export const DashboardWidget = () => {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen bg-slate-50">
       <Header />
       <div className="mx-auto max-w-screen-md py-6">
         <AvailabilityForm />
