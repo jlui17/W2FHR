3455d6bdff8e32f7c9c8d22d45bc3d93d22ea7e2
Justin Lui <justinlui17@gmail.com>
2023-02-08 15:58:22 +0000

    FrontendService redirect to index on 403 and 404

--- lib/services/FrontendService.ts
+++ lib/services/FrontendService.ts
@@ -1,6 +1,9 @@
 import { Stack } from "aws-cdk-lib";
-import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+} from "aws-cdk-lib/aws-cloudfront";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
 
@@ -11,8 +14,11 @@ export class FrontendService extends Stack {
     const siteBucket = new Bucket(this, "SiteBucket", {
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "index.html",
-      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
+
+    const oia = new OriginAccessIdentity(this, "OIA");
+    siteBucket.grantRead(oia);
 
     const siteDistribution = new CloudFrontWebDistribution(
       this,
@@ -22,8 +28,22 @@ export class FrontendService extends Stack {
           {
             s3OriginSource: {
               s3BucketSource: siteBucket,
+              originAccessIdentity: oia,
             },
             behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+        errorConfigurations: [
+          // redirect all 403 and 404 errors to index.html
+          {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: "/index.html",
+          },
+          {
+            errorCode: 404,
+            responseCode: 200,
+            responsePagePath: "/index.html",
           },
         ],
       }
@@ -33,7 +53,7 @@ export class FrontendService extends Stack {
       sources: [Source.asset("src/frontend/dist")],
       destinationBucket: siteBucket,
       distribution: siteDistribution,
-      distributionPaths: ["/*"],
+      distributionPaths: ["/", "/index.html"], // Invalidate the cache for / and index.html when we deploy so that cloudfront serves latest site
     });
   }
 }
