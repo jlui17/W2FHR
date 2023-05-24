4745249371cd341dc3c5ab3ae3523dd1072df80e
Justin Lui <justinlui17@gmail.com>
2023-05-23 21:48:57 -0700

    Timesheet Upcoming - set time zone to vancouver

--- src/GoogleSheets/packages/common/TimeService/main.go
+++ src/GoogleSheets/packages/common/TimeService/main.go
@@ -2,16 +2,31 @@ package TimeService
 
 import (
 	"GoogleSheets/packages/common/Constants/TimesheetConstants"
+	"log"
 	"time"
 )
 
+const vancouverTimeZone string = "America/Los_Angeles"
+
 func GetToday() time.Time {
-	y, m, d := time.Now().Date()
-	today := time.Date(y, m, d, 0, 0, 0, 0, time.Local)
+	vancouverTime, err := time.LoadLocation(vancouverTimeZone)
+	if err != nil {
+		log.Printf("[ERROR] Couldn't load location for time zone: %s", err.Error())
+		panic(err)
+	}
+
+	y, m, d := time.Now().In(vancouverTime).Date()
+	today := time.Date(y, m, d, 0, 0, 0, 0, vancouverTime)
 	return today
 }
 
 func ConvertDateToTime(date string) *time.Time {
-	t, _ := time.ParseInLocation(TimesheetConstants.DATE_FORMAT, date, time.Local)
+	vancouverTime, err := time.LoadLocation(vancouverTimeZone)
+	if err != nil {
+		log.Printf("[ERROR] Couldn't load location for time zone: %s", err.Error())
+		panic(err)
+	}
+
+	t, _ := time.ParseInLocation(TimesheetConstants.DATE_FORMAT, date, vancouverTime)
 	return &t
 }
