870fbf1481efae60fefd375c964da128d4d571d2
Justin Lui <justinlui17@gmail.com>
2023-06-27 19:38:21 -0700

    Auth backend fix last commit

--- src/GoogleSheets/packages/common/Constants/AuthConstants/main.go
+++ src/GoogleSheets/packages/common/Constants/AuthConstants/main.go
@@ -2,9 +2,8 @@ package AuthConstants
 
 const (
 	STAFF_LIST_SHEET_ID                = "1UWg4SPcPf6wSLNIW-u_KRt1HCHj4yhvqdQyfryNj51I"
-	EMAIL_COL                          = "G"
 	EMPLOYEE_ID_COL                    = "A"
-	EMAIL_GET_RANGE                    = "F2:F"
+	EMAIL_GET_RANGE                    = "G2:G"
 	EMPLOYEE_ID_GET_RANGE              = "A2:A"
 	STAFF_LIST_SHEET_NAME              = "Total Staff"
 	STAFF_LIST_INFO_INCONSISTENT_ERROR = "number of emails and employee ids are not the same"
