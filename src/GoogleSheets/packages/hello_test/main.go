47468154ba6ae542a47fc5a6ec121aa5f9327183
Jason Zhao <jasonnope123@gmail.com>
2023-12-20 20:43:59 -0800

    hello test function

---
+++ src/GoogleSheets/packages/hello_test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World",
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
