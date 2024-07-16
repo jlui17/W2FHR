293f8ea0767c2b9bc5f5f2293d51b9a7aa05cd9c
Jason Zhao <jasonnope123@gmail.com>
2024-07-16 01:52:14 -0700

    minor case

--- src/frontend/src/components/UpcomingShifts/UpcomingShiftsWidget.tsx
+++ src/frontend/src/components/UpcomingShifts/UpcomingShiftsWidget.tsx
@@ -1,10 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Loader2 } from "lucide-react";
 import { Shift, TimesheetData } from "../Timesheet/TimesheetController";
+import { ManagerTimesheetData } from "../Timesheet/ManagerTimesheet/ManagerTimesheetController";
 
 interface UpcomingShiftsWidgetProps {
   isLoading: boolean;
-  upcomingShiftsData: TimesheetData;
+  upcomingShiftsData: TimesheetData | ManagerTimesheetData;
 }
 
 export const UpcomingShiftsWidget = ({
@@ -22,26 +23,30 @@ export const UpcomingShiftsWidget = ({
   };
 
   const upcomingShifts = () => {
-    return (
-      <div className="mt-2 flex flex-col">
-        {upcomingShiftsData.shifts.map((shift: Shift, i: number) => {
-          return (
-            <div className="mb-6 flex flex-col" key={i}>
-              <p className="text-md">{shift.date}</p>
-              <p className="text-sm">{shift.shiftTitle}</p>
-              <p className="text-sm text-gray-600">Start: {shift.startTime}</p>
-              <p className="text-sm text-gray-600">End: {shift.endTime}</p>
-              <p className="text-sm text-gray-600">
-                Break Duration: {shift.breakDuration}
-              </p>
-              <p className="text-sm text-gray-600">
-                Net Hours: {shift.netHours}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    );
+    if (upcomingShiftsData as TimesheetData) {
+      const data = upcomingShiftsData as TimesheetData;
+
+      return (
+        <div className="mt-2 flex flex-col">
+          {data.shifts.map((shift: Shift, i: number) => {
+            return (
+              <div className="mb-6 flex flex-col" key={i}>
+                <p className="text-md">{shift.date}</p>
+                <p className="text-sm">{shift.shiftTitle}</p>
+                <p className="text-sm text-gray-600">Start: {shift.startTime}</p>
+                <p className="text-sm text-gray-600">End: {shift.endTime}</p>
+                <p className="text-sm text-gray-600">
+                  Break Duration: {shift.breakDuration}
+                </p>
+                <p className="text-sm text-gray-600">
+                  Net Hours: {shift.netHours}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
   };
 
   return (
