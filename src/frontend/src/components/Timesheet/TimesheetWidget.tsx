d3d5ef34b3f6860782bd4da2d0dee7ae1a53f143
Justin Lui <justinlui17@gmail.com>
2024-12-18 21:57:35 -0500

    Small refactoring

--- src/frontend/src/components/Timesheet/TimesheetWidget.tsx
+++ src/frontend/src/components/Timesheet/TimesheetWidget.tsx
@@ -1,13 +1,12 @@
-
 import { Button } from "@/components/ui/button";
-import { CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
-import React from "react";
+import React, { ReactElement } from "react";
 import { TimesheetData } from "@/components/Timesheet/helpers/hooks";
 
 export const TimesheetWidget = (p: {
@@ -15,18 +14,18 @@ export const TimesheetWidget = (p: {
   onOpenChange: () => void;
   isLoading: boolean;
   timesheetData: TimesheetData;
-}): JSX.Element => {
+}): ReactElement => {
   const hasNoShifts = p.timesheetData.shifts.length === 0;
 
-  const displayEmptyTimesheet = (): JSX.Element => {
+  function displayEmptyTimesheet(): ReactElement {
     return (
       <p className="m-auto text-center text-sm text-gray-600">
         You haven't worked any shifts yet
       </p>
     );
-  };
+  }
 
-  const displayTimesheet = (): JSX.Element => {
+  function displayTimesheet(): ReactElement {
     return (
       <div className="mt-2 flex flex-col">
         {p.timesheetData.shifts.map((shift, i) => {
@@ -47,37 +46,39 @@ export const TimesheetWidget = (p: {
         })}
       </div>
     );
-  };
+  }
 
   return (
-    <Collapsible
-      open={p.open}
-      onOpenChange={p.onOpenChange}
-      className="bg-card text-card-foreground w-11/12 rounded-md border bg-white shadow-sm md:w-auto"
-    >
-      <CollapsibleTrigger asChild>
-        <div className="flex items-center justify-between px-4 py-3">
-          <CardTitle className="m-auto">Shift History</CardTitle>
-          <CollapsibleTrigger asChild>
-            <Button variant="ghost" size="sm" className="w-9 p-0">
-              {p.open ? (
-                <ChevronUp className="h-4 w-4" />
-              ) : (
-                <ChevronDown className="h-4 w-4" />
-              )}
-            </Button>
-          </CollapsibleTrigger>
-        </div>
-      </CollapsibleTrigger>
-      <CollapsibleContent className="px-4 py-3 pb-7">
-        {p.isLoading ? (
-          <Loader2 className="m-auto h-16 w-16 animate-spin" />
-        ) : hasNoShifts ? (
-          displayEmptyTimesheet()
-        ) : (
-          displayTimesheet()
-        )}
-      </CollapsibleContent>
+    <Collapsible open={p.open} onOpenChange={p.onOpenChange}>
+      <Card>
+        <CollapsibleTrigger asChild>
+          <CardHeader className="transition-colors duration-200 hover:bg-gray-100">
+            <div className="flex items-center justify-between">
+              <CardTitle className="m-auto">Work History</CardTitle>
+              <CollapsibleTrigger asChild>
+                <Button variant="ghost" size="sm" className="w-9 p-0">
+                  {p.open ? (
+                    <ChevronUp className="h-4 w-4" />
+                  ) : (
+                    <ChevronDown className="h-4 w-4" />
+                  )}
+                </Button>
+              </CollapsibleTrigger>
+            </div>
+          </CardHeader>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <CardContent>
+            {p.isLoading ? (
+              <Loader2 className="m-auto h-16 w-16 animate-spin" />
+            ) : hasNoShifts ? (
+              displayEmptyTimesheet()
+            ) : (
+              displayTimesheet()
+            )}
+          </CardContent>
+        </CollapsibleContent>
+      </Card>
     </Collapsible>
   );
 };
--- src/frontend/src/components/UpcomingShifts/UpcomingShiftsWidget.tsx
+++ src/frontend/src/components/UpcomingShifts/UpcomingShiftsWidget.tsx
@@ -1,30 +1,30 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Loader2 } from "lucide-react";
 import { Shift, TimesheetData } from "@/components/Timesheet/helpers/hooks";
+import { ReactElement } from "react";
 
 interface UpcomingShiftsWidgetProps {
   isLoading: boolean;
   upcomingShiftsData: TimesheetData;
 }
 
-export const UpcomingShiftsWidget = ({
-  isLoading,
-  upcomingShiftsData,
-}: UpcomingShiftsWidgetProps): JSX.Element => {
-  const hasNoUpcomingShifts = upcomingShiftsData.shifts.length === 0;
+export function UpcomingShiftsWidget(
+  p: UpcomingShiftsWidgetProps,
+): ReactElement {
+  const hasNoUpcomingShifts: boolean = p.upcomingShiftsData.shifts.length === 0;
 
-  const noUpcomingShifts = () => {
+  function noUpcomingShifts(): ReactElement {
     return (
       <div className="mt-2 flex flex-col items-center justify-center">
         <p className="text-sm text-gray-600">You have no upcoming shifts</p>
       </div>
     );
-  };
+  }
 
-  const upcomingShifts = () => {
+  function upcomingShifts(): ReactElement {
     return (
       <div className="mt-2 flex flex-col">
-        {upcomingShiftsData.shifts.map((shift: Shift, i: number) => {
+        {p.upcomingShiftsData.shifts.map((shift: Shift, i: number) => {
           return (
             <div className="mb-6 flex flex-col" key={i}>
               <p className="text-md">{shift.date}</p>
@@ -42,7 +42,7 @@ export const UpcomingShiftsWidget = ({
         })}
       </div>
     );
-  };
+  }
 
   return (
     <Card className="w-11/12 md:w-auto">
@@ -50,7 +50,7 @@ export const UpcomingShiftsWidget = ({
         <CardTitle className="m-auto">Upcoming Shifts</CardTitle>
       </CardHeader>
       <CardContent>
-        {isLoading ? (
+        {p.isLoading ? (
           <Loader2 className="m-auto h-16 w-16 animate-spin" />
         ) : hasNoUpcomingShifts ? (
           noUpcomingShifts()
@@ -60,4 +60,4 @@ export const UpcomingShiftsWidget = ({
       </CardContent>
     </Card>
   );
-};
+}
