49af41bfea533672f5a7191265120c1c1a7e07ed
Justin Lui <justinlui17@gmail.com>
2024-12-23 19:03:06 -0800

    Add EmployeeNamesAndIds to Schedule Metadata

--- src/GoogleSheets/packages/schedule/Schedule/ScheduleTypes.go
+++ src/GoogleSheets/packages/schedule/Schedule/ScheduleTypes.go
@@ -1,7 +1,8 @@
 package Schedule
 
 type Metadata struct {
-	ShiftTitles    []string `json:"shiftTitles"`
-	ShiftTimes     []string `json:"shiftTimes"`
-	BreakDurations []string `json:"breakDurations"`
+	ShiftTitles         []string `json:"shiftTitles"`
+	ShiftTimes          []string `json:"shiftTimes"`
+	BreakDurations      []string `json:"breakDurations"`
+	EmployeeNamesAndIds []string `json:"employeeNamesAndIds"`
 }
