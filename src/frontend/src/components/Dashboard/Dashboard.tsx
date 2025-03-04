4f039d1565813672c449cb7aaaac088d3c89b071
Justin Lui <justinlui17@gmail.com>
2025-03-03 21:45:58 -0800

    Make desktop view xl

--- src/frontend/src/components/Dashboard/Dashboard.tsx
+++ src/frontend/src/components/Dashboard/Dashboard.tsx
@@ -36,7 +36,7 @@ export const Dashboard = () => {
   return (
     <div className="h-full w-screen bg-slate-50">
       <Header />
-      <div className="mx-auto flex h-full min-h-screen max-w-screen-md flex-col content-start items-center justify-start gap-x-10 gap-y-6 p-6 lg:grid lg:max-w-screen-lg lg:grid-cols-2 lg:items-start lg:justify-center">
+      <div className="mx-auto flex h-full min-h-screen max-w-screen-md flex-col content-start items-center justify-start gap-x-10 gap-y-6 p-6 lg:grid lg:max-w-screen-xl lg:grid-cols-2 lg:items-start lg:justify-center">
         <QueryClientProvider client={DashboardQueryClient}>
           <Availability />
           <UpcomingShifts />
